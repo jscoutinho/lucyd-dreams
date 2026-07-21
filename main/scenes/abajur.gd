1338ef98ff8849138becb5230d1aa0961403fef9
João Coutinho <126711095+jscoutinho@users.noreply.github.com>
2026-07-21 18:11:47 -0300

    refatorei código lucy maquina de estados

--- main/scenes/abajur.gd
+++ main/scenes/abajur.gd
@@ -1,14 +1,17 @@
 extends Node2D
 
 @onready var prompt = $Prompt
+@onready var label: Label = $Label
 
 func _ready():
 	prompt.hide()
+	label.hide()
 
 func _on_area_2d_body_entered(body):
 	if body.name == "Lucy":
 		prompt.show()
-
+		label.show()
 func _on_area_2d_body_exited(body):
 	if body.name == "Lucy":
 		prompt.hide()
+		label.show()
--- main/scripts/player.gd
+++ main/scripts/player.gd
@@ -1,51 +1,148 @@
 extends CharacterBody2D
+
+enum PlayerState {
+	IDLE,
+	TRANSITION,
+	WALK,
+	JUMP
+}
+
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 
-
 const SPEED = 300.0
-const JUMP_VELOCITY = -500.0
-var transitioning = false
+const JUMP_VELOCITY = -760.0
+const GRAVITY_MULTIPLIER = 1.5
+
+var status: PlayerState
+
+
+func _ready() -> void:
+	go_to_idle_state()
+
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		velocity += get_gravity() * GRAVITY_MULTIPLIER * delta
 
-	# Handle jump.
+	match status:
+		PlayerState.IDLE:
+			idle_state()
+
+		PlayerState.TRANSITION:
+			transition_state()
+
+		PlayerState.WALK:
+			walk_state()
+
+		PlayerState.JUMP:
+			jump_state()
+
+	move_and_slide()
+
+
+#==========================
+# TROCA DE ESTADOS
+#==========================
+
+func go_to_idle_state():
+	status = PlayerState.IDLE
+	anim.play("idle")
+
+
+func go_to_transition_state():
+	status = PlayerState.TRANSITION
+	anim.play("transitioning")
+
+
+func go_to_walk_state():
+	status = PlayerState.WALK
+	anim.play("walking")
+
+
+func go_to_jump_state():
+	status = PlayerState.JUMP
+	anim.play("jump")
+	velocity.y = JUMP_VELOCITY
+
+
+#==========================
+# ESTADOS
+#==========================
+
+func idle_state():
+	move()
+
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		go_to_jump_state()
+		return
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	if velocity.x != 0:
+		go_to_transition_state()
+		return
+
+
+func transition_state():
+	move()
+
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		go_to_jump_state()
+		return
+
+	# Espera a animação terminar
+	if velocity.x == 0:
+		go_to_idle_state()
+
+
+func walk_state():
+	move()
+
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		go_to_jump_state()
+		return
+
+	if velocity.x == 0:
+		go_to_idle_state()
+		return
+
+
+func jump_state():
+	move()
+
+	if is_on_floor():
+		if velocity.x == 0:
+			go_to_idle_state()
+		else:
+			go_to_walk_state()
+
+
+#==========================
+# MOVIMENTO
+#==========================
+
+func move():
 	var direction := Input.get_axis("ui_left", "ui_right")
+
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-	
-	
+
 	if direction > 0:
 		anim.flip_h = false
-		if !transitioning and anim.animation != "walking":
-			transitioning = true
-			anim.play("transitioning")
 	elif direction < 0:
 		anim.flip_h = true
-		if !transitioning and anim.animation != "walking":
-			transitioning = true
-			anim.play("transitioning")
-	else:
-		transitioning = false
-		anim.play("idle")
-	move_and_slide()
 
 
-
-
+#==========================
+# ANIMAÇÕES
+#==========================
 
 func _on_animated_sprite_2d_animation_finished() -> void:
 	if anim.animation == "transitioning":
-		if Input.get_axis("ui_left", "ui_right") != 0:
-			anim.play("walking")
-		else:
-			anim.play("idle")
+		if status == PlayerState.TRANSITION:
+			if velocity.x != 0:
+				go_to_walk_state()
+			else:
+				go_to_idle_state()
