47464d8d9b4fc4a2afac3cd7fdc3280b7b06d464
João Coutinho <126711095+jscoutinho@users.noreply.github.com>
2026-07-27 15:21:37 -0300

    acabei a arte da sala fazendo as interações

---
+++ main/scripts/objects/papel.gd
@@ -0,0 +1,46 @@
+extends Node2D
+
+@onready var interact: AnimatedSprite2D = $interact
+@onready var papeln: Label = $PapelN
+
+const FOTO := preload("res://assets/intro/img-2.png")
+
+var player_near = false
+var interacting = false
+
+func _ready() -> void:
+	interact.hide()
+	papeln.hide()
+
+func _process(_delta: float) -> void:
+	if player_near and Input.is_action_just_pressed("interact") and !interacting:
+		interacting = true
+		interact.play("press")
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.name == "Lucy":
+		player_near = true
+		interact.show()
+		interact.play("idle")
+		papeln.show()
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.name == "Lucy":
+		player_near = false
+		interact.hide()
+		papeln.hide()
+		interacting = false
+		
+
+
+func _on_interact_animation_finished() -> void:
+	match interact.animation:
+		"press":
+			var dialogue = get_tree().current_scene.get_node("UI/ImageBox")
+			dialogue.show_dialogue(["Parece que alguém deixou esse papel no sofá.","Ah... Esse dia no parque. Me lembro como se fosse ontem.", "Nessa época, eu não sabia o quão sortuda era. Queria tantas coisas, mas não enxerguei que já tinha tudo o que eu precisava.", "Obrigada por me obrigar a tirar essa foto."], "Lucy", "res://assets/jogo/casa/documento.png")
+			interact.play("release")
+
+		"release":
+			interact.play("idle")
+			interacting = false
